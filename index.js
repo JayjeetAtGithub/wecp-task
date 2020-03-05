5ca37227d34dc526fe56d7d516fce38fce3c2c52
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-03-05 09:38:14 +0530

    Correct variable naming

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const spawn = require('child_process').spawn;
 const { Readable } = require('stream')
 const fs = require('fs');
 
-const INPUTS_FILE_PATH = "input.txt"
-const OUTPUTS_FILE_PATH = "output.txt"
+const INPUT_FILE_PATH = "input.txt"
+const OUTPUT_FILE_PATH = "output.txt"
 
 // touch the empty input and output files
-fs.closeSync(fs.openSync(INPUTS_FILE_PATH, 'w'));
-fs.closeSync(fs.openSync(OUTPUTS_FILE_PATH, 'w'));
+fs.closeSync(fs.openSync(INPUT_FILE_PATH, 'w'));
+fs.closeSync(fs.openSync(OUTPUT_FILE_PATH, 'w'));
 
 // function to generate a random number in a given range
 function get_rand(min, max) {
@@ -18,13 +18,13 @@ function get_rand(min, max) {
 }
 
 // function to generate the inputs of the test cases
-function generate_test_case(ube, lbe, ubne, lbne) {
-        let num_count = get_rand(lbne, ubne);
-        let num_array = new Array();
-	for (let i = 0; i < num_count; i++) {
-		num_array.push(get_rand(lbe, ube));
+function generate_test_case(lbae, ubae, lbal, ubal) {
+        let arr_len = get_rand(lbal, ubal);
+        let arr_elements = new Array();
+	for (let i = 0; i < arr_len; i++) {
+		arr_elements.push(get_rand(lbae, ubae));
         }
-        test_cases.push({ 'n': num_count, 'elements': num_array });
+        test_cases.push({ 'n': arr_len, 'elements': arr_elements });
 }
 
 // function to run the binary on the input, get the output, write to file
@@ -34,32 +34,32 @@ function run_binary_and_generate_output(input_string, binary_path) {
         input_stream.pipe(proc.stdin);
 
         proc.stdout.on('data', function(data) {
-                fs.appendFileSync(INPUTS_FILE_PATH, input_string + "\n");
-                fs.appendFileSync(OUTPUTS_FILE_PATH, data.toString('ascii'));
+                fs.appendFileSync(INPUT_FILE_PATH, input_string + "\n");
+                fs.appendFileSync(OUTPUT_FILE_PATH, data.toString('ascii'));
         });
 }
 
 // take the inputs of the constraints
 const num_test_cases = parseInt(prompt('Number of test cases to generate: '));
-const upper_bound_num_elements = parseInt(prompt('Upper bound of no. of elements: '));
-const lower_bound_num_elements = parseInt(prompt('Lower bound of no. of elements: '));
-const upper_bound_element = parseInt(prompt('Upper bound of element: '));
-const lower_bound_element = parseInt(prompt('Lower bound of element: '));
-const binary_path = prompt('Path to the binary: ');
+const upper_bound_arr_len = parseInt(prompt('Upper bound of no. of elements: '));
+const lower_bound_arr_len = parseInt(prompt('Lower bound of no. of elements: '));
+const upper_bound_arr_element = parseInt(prompt('Upper bound of element: '));
+const lower_bound_arr_element = parseInt(prompt('Lower bound of element: '));
+const path_to_binary = prompt('Path to the binary: ');
 
 // generate the inputs of the test cases
 test_cases = new Array();
 for (let i = 0; i < num_test_cases; i++) {
 	generate_test_case(
-                upper_bound_element, 
-                lower_bound_element, 
-                upper_bound_num_elements, 
-                lower_bound_num_elements
+                upper_bound_arr_element, 
+                lower_bound_arr_element, 
+                upper_bound_arr_len, 
+                lower_bound_arr_len
         );
 }
 
 // generate the outputs of the corresponding inputs of the test cases
 for (let idx = 0; idx < test_cases.length; idx++) {
         let input_string = test_cases[idx].n + " " + test_cases[idx].elements.join(" ");
-        run_binary_and_generate_output(input_string, binary_path);
+        run_binary_and_generate_output(input_string, path_to_binary);
 }
